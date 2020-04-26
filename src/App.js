9137fe1c7b935185ae79d569b2158d3cc8e3c86f
kwiaciu <bkwiatek3@gmail.com>
2020-04-26 19:06:35 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef} from 'react';
 import './App.css';
+import * as io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  // w celu uzycia wartosci w useEffect musze skorzystac z referencji tworzonej przez useRef
+  const connectionRef = useRef();
+  const messagesRef = useRef([]);
+
+  const [messages, setMessages] = useState([]);
+  const [myMessage, setMyMessages] = useState(messagesRef.current);
+
+  useEffect(() => {
+    // sprawdzam czy polaczenie zostalo juz nawiazane
+    if (connectionRef.current === undefined) {
+      // nawiazuje polaczenie
+      connectionRef.current = io.connect('https://chat-server.fbg.pl');
+      connectionRef.current.on('chat message', (message) => {
+        messagesRef.current = [...messagesRef.current, message];
+        setMessages(messagesRef.current);
+      })
+    }}, []);
+
+  const handleMessageChange = (event) => {
+    setMyMessages(event.target.value)
+  }
+
+  const handleSend = () => {
+    connectionRef.current.emit('chat message', {
+      text: myMessage,
+      authorId: 'Bartek',
+    });
+  };
+  return (<>
+    <div>
+      <input onChange={handleMessageChange} value={myMessage} />
+      <button onClick={handleSend}>Send</button>
     </div>
+    <ul>
+      {messages.map((item, index) => <li key={index}><b>{item.authorId}:</b> {item.text}</li>)}
+    </ul>
+  </>
   );
 }
 
